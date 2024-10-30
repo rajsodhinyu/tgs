6fcb9880d13a5de120dddb931860a578dcaf3b59
rajsodhinyu <rs7152@nyu.edu>
2024-10-30 09:32:53 -0400

    added two fonts, made nav stretchier

--- next-js/src/app/blog/page.tsx
+++ next-js/src/app/blog/page.tsx
@@ -5,9 +5,9 @@ import Card from "./blogCard";
 export default function Page(){
   return (
     <div>
-      <h1 className=" bg-lime-400">Blog Home</h1>
+      <h1 className="font-bold font-bit text-2xl pt-2 ">Blog Home</h1>
         <div className="grid grid-cols-3 p-8">
-          <div className="flex text-center">
+          <div className="flex text-center font-roc">
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus aliquam dolor, eu lacinia lorem placerat vulputate. Duis felis orci, pulvinar id metus ut, rutrum luctus orci. Cras porttitor imperdiet nunc, at ultricies tellus laoreet sit amet. Sed auctor cursus massa at porta. Integer ligula ipsum, tristique sit amet orci vel, viverra egestas ligula. Curabitur vehicula tellus neque, ac ornare ex malesuada et. In vitae convallis lacus. Aliquam erat volutpat. Suspendisse ac imperdiet turpis. Aenean finibus sollicitudin eros pharetra congue. Duis ornare egestas augue ut luctus. Proin blandit quam nec lacus varius commodo et a urna. Ut id ornare felis, eget fermentum sapien.</p>
           </div>
           <div></div>
@@ -15,8 +15,6 @@ export default function Page(){
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus aliquam dolor, eu lacinia lorem placerat vulputate. Duis felis orci, pulvinar id metus ut, rutrum luctus orci. Cras porttitor imperdiet nunc, at ultricies tellus laoreet sit amet. Sed auctor cursus massa at porta. Integer ligula ipsum, tristique sit amet orci vel, viverra egestas ligula. Curabitur vehicula tellus neque, ac ornare ex malesuada et. In vitae convallis lacus. Aliquam erat volutpat. Suspendisse ac imperdiet turpis. Aenean finibus sollicitudin eros pharetra congue. Duis ornare egestas augue ut luctus. Proin blandit quam nec lacus varius commodo et a urna. Ut id ornare felis, eget fermentum sapien.</p>
           </div>
         </div>
-      
-    
     </div>
   )
 } 
