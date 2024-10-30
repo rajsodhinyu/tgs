d3594405e4d9e9b48b5a04413eebc95d1d256da6
rajsodhinyu <rs7152@nyu.edu>
2024-10-30 13:30:35 -0400

    fixed padding/size on /about

--- next-js/src/app/about/page.tsx
+++ next-js/src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
-    return (<div className="w-full h-screen place-content-center bg-gradient-to-r">
+    return (<div className="w-full h-full place-content-center bg-gradient-to-r p-64">
         <div className="text-center">
-            <p className="text-4xl font-black font-bit px-11 text-balance"> That Good Sh*t! because good music transcends genre! Our mission is to build community around shared love of music. Through artist interviews, curated live events, playlists, and more, TGS is your go-to source for keeping up with the most important and innovative music of today!</p>
+            <p className="text-5xl font-black font-bit  text-balance"> That Good Sh*t! because good music transcends genre! Our mission is to build community around shared love of music. Through artist interviews, curated live events, playlists, and more, TGS is your go-to source for keeping up with the most important and innovative music of today!</p>
         </div>
     </div>)
 }
