ed5163e5383bd9b69fcc0954a7b38649aedcd7b0
rajsodhinyu <rs7152@nyu.edu>
2024-11-10 15:48:35 -0500

    changed SOTD widget scaling and added about text

--- next-js/src/app/about/page.tsx
+++ next-js/src/app/about/page.tsx
@@ -1,7 +1,7 @@
 export default function Page() {
-    return (<div className="w-full h-full place-content-center bg-gradient-to-r p-64">
-        <div className="text-center">
-            <p className="text-5xl font-black font-bit  text-balance"> That Good Sh*t! because good music transcends genre! Our mission is to build community around shared love of music. Through artist interviews, curated live events, playlists, and more, TGS is your go-to source for keeping up with the most important and innovative music of today!</p>
+    return (<div className="bg-gradient-to-r px-40 h-full w-full">
+        <div className="text-5xl font-black font-bit text-center p-40 text-balance">
+            <p> That Good Sh*t! because good music transcends genre! Our mission is to build community around shared love of music. Through artist interviews, curated live events, playlists, and more, TGS is your go-to source for keeping up with the most important and innovative music of today!</p>
         </div>
     </div>)
 }
