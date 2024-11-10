40ed15b1feb0a4e4e993d0e58cbaa6698f75645d
rajsodhinyu <rs7152@nyu.edu>
2024-11-10 16:44:16 -0500

    working on /blog

--- next-js/src/app/blog/page.tsx
+++ next-js/src/app/blog/page.tsx
@@ -1,20 +1,25 @@
 import Image from "next/image"
 import Card from "./blogCard";
 
-
-export default function Page(){
+export default function Page() {
   return (
-    <div>
-      <h1 className="font-bold font-bit text-2xl pt-2 ">Blog Home</h1>
-        <div className="grid grid-cols-3 p-8">
-          <div className="flex text-center font-roc">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus aliquam dolor, eu lacinia lorem placerat vulputate. Duis felis orci, pulvinar id metus ut, rutrum luctus orci. Cras porttitor imperdiet nunc, at ultricies tellus laoreet sit amet. Sed auctor cursus massa at porta. Integer ligula ipsum, tristique sit amet orci vel, viverra egestas ligula. Curabitur vehicula tellus neque, ac ornare ex malesuada et. In vitae convallis lacus. Aliquam erat volutpat. Suspendisse ac imperdiet turpis. Aenean finibus sollicitudin eros pharetra congue. Duis ornare egestas augue ut luctus. Proin blandit quam nec lacus varius commodo et a urna. Ut id ornare felis, eget fermentum sapien.</p>
-          </div>
-          <div></div>
-          <div className="flex text-center">
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla luctus aliquam dolor, eu lacinia lorem placerat vulputate. Duis felis orci, pulvinar id metus ut, rutrum luctus orci. Cras porttitor imperdiet nunc, at ultricies tellus laoreet sit amet. Sed auctor cursus massa at porta. Integer ligula ipsum, tristique sit amet orci vel, viverra egestas ligula. Curabitur vehicula tellus neque, ac ornare ex malesuada et. In vitae convallis lacus. Aliquam erat volutpat. Suspendisse ac imperdiet turpis. Aenean finibus sollicitudin eros pharetra congue. Duis ornare egestas augue ut luctus. Proin blandit quam nec lacus varius commodo et a urna. Ut id ornare felis, eget fermentum sapien.</p>
-          </div>
-        </div>
+  <div className="bg-white justify-between items-start inline-flex"> {/* Whole Site */}
+    <div className="grow shrink basis-0 self-stretch flex-col justify-start items-center gap-2.5 inline-flex"> {/* Right Side, Carousel + */}
+      <div className="basis-0 py-96 justify-center items-center gap-2.5 inline-flex">
+        <div><img src="https://via.placeholder.com/1920x1080" alt="" /></div>
+      </div>
     </div>
+    <div className=" ">
+      <div className="text-center text-[#212121] text-3xl font-bit leading-10 tracking-widest">Latest Stories</div>
+      <img className="w-80 h-96 p-2.5 shadow border border-[#6f5bc4]" src="https://via.placeholder.com/335x369" />
+      <img className="w-80 h-96 p-2.5 shadow border border-[#6f5bc4]" src="https://via.placeholder.com/335x369" />
+      <div className="w-80 h-96 p-2.5 bg-[#65b9e7] shadow border border-[#6f5bc4]"></div>
+      <img className="w-80 h-96 p-2.5 shadow border border-[#6f5bc4]" src="https://via.placeholder.com/335x369" />
+      <img className="w-80 h-96 p-2.5 shadow border border-[#6f5bc4]" src="https://via.placeholder.com/335x369" />
+      <div className="w-80 h-96 p-2.5 bg-[#65b9e7] shadow border border-[#6f5bc4]"></div>
+    </div>
+  </div>
   )
-} 
+}
+
+
