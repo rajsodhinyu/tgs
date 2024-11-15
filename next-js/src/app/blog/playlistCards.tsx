1d03814a882e57c6708129f86cdff9e2b1b554dd
rajsodhinyu <rs7152@nyu.edu>
2024-11-15 02:21:40 -0500

    changing spacing with playlists

--- next-js/src/app/blog/playlistCards.tsx
+++ next-js/src/app/blog/playlistCards.tsx
@@ -4,13 +4,13 @@ import Link from "next/link";
 
 export default function PlaylistCard({ title, description, cover, url }: { title: string, description: string ,cover: string, url:string }) {
   return (
-    <div className=" align-middle pb-6 ">
+    <div className=" align-middle pb-3 ">
      <Link className="hover:underline"
             href={url}> 
         <img className='rounded-xl place-self-center' src={cover} alt="" width={225} height={225}/>
       <div className="flex-col justify-start">
-        <div className="pt-2 text-center text-black text-2xl font-medium font-bit leading-7">{title}</div>
-        <div className="text-center text-black text-balance text-sm font-semibold font-roc leading-none pt-1">{description}</div>
+        <div className="pt-4 text-center text-pretty text-black text-3xl font-medium font-bit leading-7">{title}</div>
+        <div className="text-center text-black text-pretty text-sm font-semibold font-roc leading-none pt-2">{description}</div>
       </div>
       </Link>
     </div>
