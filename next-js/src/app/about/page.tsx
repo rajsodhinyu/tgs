448ddce044ea9e7480edd145e299275fdea84dca
rajsodhinyu <rs7152@nyu.edu>
2024-11-27 20:41:45 -0500

    about image in lg

--- next-js/src/app/about/page.tsx
+++ next-js/src/app/about/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
         <div className="flex sm:text-2xl text-lg font-black font-bit text-pretty text-center sm:text-balance">
             <p> That Good Sh*t! because good music transcends genre! Our mission is to build community around shared love of music. Through artist interviews, curated live events, playlists, and more, TGS is your go-to source for keeping up with the most important and innovative music of today!</p>
         </div>
-        <div className="flex">
+        <div className="flex lg:mt-10">
             <img src="https://cdn.sanity.io/images/fnvy29id/tgs/b5cee228b7299f1fd664e36a34f48678a30cb3d0-1250x1000.jpg" alt="" />
         </div>
     </div>
