cede959447f863ab93348f9156e81325d4d21ccf
rajsodhinyu <rs7152@nyu.edu>
2024-12-01 23:21:49 -0500

    tried wrapping in a separate file

---
+++ next-js/src/app/backround.tsx
@@ -0,0 +1,68 @@
+"use client"
+import * as React from 'react'
+import p5 from 'p5';
+
+export const MyComp = () => {
+    React.useEffect(() => {
+        //window is accessible here
+        console.log("window.innerHeight", window.innerHeight);
+        const s = (s: p5) => {
+
+            let width = s.windowWidth;
+            let height = s.windowHeight * 0.86;
+            let trueHeight = s.windowHeight;
+
+            let flower = {
+                x: 200,
+                y: 100,
+                emoji: '🌸'
+            };
+            let myFlower = createFlower();
+            let x = 0
+            let y = 0
+            function createFlower() {
+                // Define a flower object.
+                let flower = {
+                    x: s.random(0, width),
+                    y: s.random(0, height),
+                    size: s.random(20, 75),
+                    lifespan: s.random(255, 300),
+                    color: s.color(s.random(255), s.random(255), s.random(255)),
+                };
+                // Return the flower object.
+                return flower;
+            }
+
+            s.preload = () => {
+
+            }
+            s.setup = () => {
+                s.createCanvas(s.windowWidth, s.windowHeight * 0.86);
+                s.background(0);
+                s.frameRate(1);
+
+
+            };
+
+            s.draw = () => {
+                let flowers = ["Rose", "Daisy", "Tulip"];
+                s.text(flower.emoji, flower.x, flower.y);
+
+                s.ellipse(myFlower.x, myFlower.y, myFlower.size);
+                s.text(flower.emoji, flower.x, flower.y);
+                for (let i = 0; i < 3; i += 1) {
+                    let myFlower = createFlower();
+                    s.ellipse(myFlower.x, myFlower.y, myFlower.size);
+                    s.fill(myFlower.color);
+                    s.text(flower.emoji, flower.x, flower.y);
+                    s.remove
+                }
+
+            };
+        };
+        const myP5 = new p5(s);
+        return myP5.remove;
+    }, [])
+
+    return (<div></div>)
+}
--- next-js/src/app/page.tsx
+++ next-js/src/app/page.tsx
@@ -1,77 +1,18 @@
 "use client"
 
-import { useState, useEffect } from 'react'
+import * as React from 'react'
 import p5, { ANGLE_MODE } from 'p5';
+import { MyComp } from './backround';
+
 
 
 
 export default function Page() {
   
-  useEffect(() => {
-
-  
-
-  const s = ( s:p5 ) => {
-
-    let width = s.windowWidth;
-    let height = s.windowHeight*0.86;
-    let trueHeight = s.windowHeight;
-
-    let flower = { 
-      x: 200, 
-      y: 100, 
-      emoji: '🌸'
-    };
-    let myFlower = createFlower();
-    let x = 0
-      let y = 0
-      function createFlower() {
-        // Define a flower object.
-        let flower = {
-          x: s.random(0, width),
-          y: s.random(0, height),
-          size: s.random(20, 75),
-          lifespan: s.random(255, 300),
-          color: s.color(s.random(255), s.random(255), s.random(255)),
-        };
-        // Return the flower object.
-        return flower;
-      }
-      
-    s.preload = () => {
-
-    }
-    s.setup = () => {
-      s.createCanvas(s.windowWidth,s.windowHeight*0.86);
-      s.background(0);
-      s.frameRate(1);
-      
-      
-    };
-  
-    s.draw = () => {
-      let flowers = ["Rose", "Daisy", "Tulip"];
-      s.text(flower.emoji, flower.x, flower.y);
-      
-      s.ellipse(myFlower.x, myFlower.y, myFlower.size);
-      s.text(flower.emoji, flower.x, flower.y);
-      for (let i = 0; i < 3; i += 1) {
-        let myFlower = createFlower();
-        s.ellipse(myFlower.x, myFlower.y, myFlower.size);
-        s.fill(myFlower.color);
-        s.text(flower.emoji, flower.x, flower.y);
-        s.remove
-      }
-
-    };
-  };
-
-    const myP5 = new p5(s);
-    return myP5.remove;
-  },[])
   return (
     <div >
       <div>
+      <MyComp></MyComp>
       </div>
     </div>
   );
