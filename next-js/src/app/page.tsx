0d8f9640b750a27709fd73b434e9c93f710530c3
rajsodhinyu <rs7152@nyu.edu>
2024-12-02 18:05:06 -0500

    took out the about sign and made the graphic a spiral + one fix to make it disappear with path tracing

--- next-js/src/app/page.tsx
+++ next-js/src/app/page.tsx
@@ -32,21 +32,16 @@ export default function Page() {
               />
             </Link></div>
           <div className='font-title m-10 absolute bottom-0 left-0 text-white z-40 text-8xl'>
-            <Link href="/about">
-              <img
-                className="h-16 md:h-28 min-w-12"
-                src="https://cdn.sanity.io/images/fnvy29id/tgs/2db78d790b6062d7cb293b895f1d8cd3748353ef-1786x755.png?h=200"
-              />
-            </Link></div>
+            </div>
         </div>
-        <div className='font-title m-10 absolute top-0 right-0 text-white z-40 text-8xl'>
+        <div className='font-title m-10 absolute top-0 right-0 text-white z-40 text-8xl flex-col justify-around'>
           <Link href="/blog">
             <img
               className="h-16 md:h-28 min-w-10"
               src="https://cdn.sanity.io/images/fnvy29id/tgs/9a5244d1c770d1e667006b7f54f5738745847917-1533x846.png?h=200"
             />
           </Link></div>
-        <div className='font-title m-10 absolute right-0 text-white z-40 my-80 pt-28 text-8xl'>
+        <div className='font-title m-10 absolute inset-x-0 bottom-0 text-white z-40 text-8xl justify-center'>
           <Link href="/events">
             <img
               className="h-16 md:h-28 min-w-16"
