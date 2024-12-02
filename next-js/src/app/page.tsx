ba713a8d3b555bae64766bcb63d5ce864d9443f6
rajsodhinyu <rs7152@nyu.edu>
2024-12-01 23:12:03 -0500

    big test for p5 on the homepage

--- next-js/src/app/page.tsx
+++ next-js/src/app/page.tsx
@@ -1,13 +1,79 @@
-import Backround from "./backround";
+"use client"
+
+import { useState, useEffect } from 'react'
+import p5, { ANGLE_MODE } from 'p5';
+
 
 
 export default function Page() {
-    return (<div>wait
+  
+  
 
-        they dont love u like i love you 
-        <Backround></Backround>
-        wait 
+  const s = ( s:p5 ) => {
 
-    </div>);
+    let width = s.windowWidth;
+    let height = s.windowHeight*0.86;
+    let trueHeight = s.windowHeight;
+
+    let flower = { 
+      x: 200, 
+      y: 100, 
+      emoji: '🌸'
+    };
+    let myFlower = createFlower();
+    let x = 0
+      let y = 0
+      function createFlower() {
+        // Define a flower object.
+        let flower = {
+          x: s.random(0, width),
+          y: s.random(0, height),
+          size: s.random(20, 75),
+          lifespan: s.random(255, 300),
+          color: s.color(s.random(255), s.random(255), s.random(255)),
+        };
+        // Return the flower object.
+        return flower;
+      }
+      
+    s.preload = () => {
+
+    }
+    s.setup = () => {
+      s.createCanvas(s.windowWidth,s.windowHeight*0.86);
+      s.background(0);
+      s.frameRate(1);
+      
+      
+    };
+  
+    s.draw = () => {
+      let flowers = ["Rose", "Daisy", "Tulip"];
+      s.text(flower.emoji, flower.x, flower.y);
+      
+      s.ellipse(myFlower.x, myFlower.y, myFlower.size);
+      s.text(flower.emoji, flower.x, flower.y);
+      for (let i = 0; i < 3; i += 1) {
+        let myFlower = createFlower();
+        s.ellipse(myFlower.x, myFlower.y, myFlower.size);
+        s.fill(myFlower.color);
+        s.text(flower.emoji, flower.x, flower.y);
+        s.remove
+      }
+
+    };
+  };
+
+  useEffect(() => {
+    const myP5 = new p5(s);
+    return myP5.remove;
+  },[])
+  return (
+    <div >
+      <div>
+      </div>
+    </div>
+  );
+
 }
 
