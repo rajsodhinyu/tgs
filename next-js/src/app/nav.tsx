dc01474d52e9c2876cba8e2da877046fccddf96f
rajsodhinyu <rs7152@nyu.edu>
2024-12-03 02:11:16 -0500

    made SOTD link to home for now

--- next-js/src/app/nav.tsx
+++ next-js/src/app/nav.tsx
@@ -40,12 +40,14 @@ export default function Nav() {
             />
           </Link>
         </div>
+        <Link href="/">
         <div className="pt-3 md:pt-0">
           <div className="w-72 xl:w-72 md:ml-4 xl:flex-none rounded-xl inline-flex place-items-center align-center justify-around">
           
             <img src="https://cdn.sanity.io/images/fnvy29id/tgs/9d324ccb04867fdad82cf73515eb8f1461570d31-2010x516.png" alt="" />
           </div>
         </div>
+        </Link>
       </div>
     </main>
   );
