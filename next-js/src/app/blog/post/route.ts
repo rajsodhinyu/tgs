5a756db81a7c2218bfd941baf78b24078959a5aa
rajsodhinyu <rs7152@nyu.edu>
2024-12-03 12:15:06 -0500

    made construction

--- next-js/src/app/blog/post/route.ts
+++
@@ -1,8 +0,0 @@
-import { redirect } from 'next/navigation'
-
-
-
-export async function GET(request: Request) {
-  
-  redirect('/blog')
-}
--- next-js/src/app/ui/fonts.ts
+++
@@ -1,1 +0,0 @@
-import { Micro_5 } from 'next/font/google';
