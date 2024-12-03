b8a8568b502c9fef9ac42ea71db643e61950f6db
rajsodhinyu <rs7152@nyu.edu>
2024-12-03 02:22:11 -0500

    pretty mobile text

--- next-js/src/app/events/page.tsx
+++ next-js/src/app/events/page.tsx
@@ -1,11 +1,11 @@
 export default function Post() {
   return (<div className="flex justify-center md:justify-normal *:w-full *:text-4xl">
      
-      <div className="ml-10 font-bit  text-center">
-      <div className="text-center">No upcoming events yet! </div>
-      <div className="text-center hover:underline"><a href="sms:+18055002436">Text +1-(805)-500-2436 to be the first to know...</a></div>
+      <div className="ml-10 font-bit  text-center text-pretty">
+      <div className="text-center text-pretty">No upcoming events yet! </div>
+      <div className="text-center text-pretty hover:underline"><a href="sms:+18055002436">Text +1-(805)-500-2436 to be the first to know...</a></div>
       <br />
-      <a className="ml-10 font-bit text-5xl text-center hover:underline" href="/events/archive"> Check out the Events Archive &gt;</a>
+      <a className="ml-10 font-bit text-5xl text-center text-pretty hover:underline" href="/events/archive"> Check out the Events Archive &gt;</a>
       </div>
   </div>)
 }
