9ed1576df414f2e9123fe1c6db0f08e9fc47a40a
rajsodhinyu <rs7152@nyu.edu>
2024-12-03 21:51:51 -0500

    mopbile fix

--- next-js/src/app/blog/layout.tsx
+++ next-js/src/app/blog/layout.tsx
@@ -4,12 +4,12 @@ import Sidebar from "./sidebar";
 export default function Layout({ children }: { children: React.ReactNode }) {
     return (<div>
         <Nav />
-        <div className="px-3 sm:flex-wrap gap-2.5  md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center "> 
+        <div className="px-3 sm:flex-wrap gap-2.5 md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center "> 
   {/* Whole Site */}
     <div className="w-full"> {/* Right Side, Carousel + */} 
       {children}
     </div>
-    <div className="w-full md:w-9/12 lg:w-8/12 xl:w-3/12 flex-col place-items-center justify-end rounded-xl"> {/* Left Side */}
+    <div className="w-full md:w-9/12 lg:w-8/12 xl:w-3/12 flex-col place-content-center rounded-xl"> {/* Left Side */}
     <Sidebar items={6}></Sidebar>
     </div>
   </div>
