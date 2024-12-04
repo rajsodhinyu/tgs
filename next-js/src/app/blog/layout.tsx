a174b3d4687428eab25ceb1463ec4190aa59d5f9
rajsodhinyu <rs7152@nyu.edu>
2024-12-03 21:33:38 -0500

    mobile width full

--- next-js/src/app/blog/layout.tsx
+++ next-js/src/app/blog/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <Nav />
         <div className="px-3 sm:flex-wrap gap-2.5  md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center "> 
   {/* Whole Site */}
-    <div className="w-11/12"> {/* Right Side, Carousel + */} 
+    <div className="w-full"> {/* Right Side, Carousel + */} 
       {children}
     </div>
     <div className="sm:w-[90vw] md:w-9/12 lg:w-8/12 xl:w-3/12 flex-col place-items-center justify-end rounded-xl"> {/* Left Side */}
