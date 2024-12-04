5f0d542d946a278013f5cef184f7f78c1b72875b
rajsodhinyu <rs7152@nyu.edu>
2024-12-03 21:09:17 -0500

    changed events text

--- next-js/src/app/events/page.tsx
+++ next-js/src/app/events/page.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link"
+
 export default function Post() {
-  return (<div className="flex justify-center md:justify-normal *:w-full *:text-4xl">
-     
-      <div className="ml-10 font-bit  text-center text-pretty">
-      <div className="text-center text-pretty">No upcoming events yet! </div>
-      <div className="text-center text-pretty hover:underline"><a href="sms:+18055002436">Text +1-(805)-500-2436 to be the first to know...</a></div>
+  return (<div className='font-title text-4xl text-center *:text-balance absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2'>
+    <div className="text-2xl">
+      No upcoming events for now!
       <br />
-      <a className="ml-10 font-bit text-5xl text-center text-pretty hover:underline" href="/events/archive"> Check out the Events Archive &gt;</a>
-      </div>
+      <Link className="hover:underline" href="sms:+18055002436">Text +1-(805)-500-2436 to be the first to know...</Link>
+    </div>
+    <br />
+    <div className="text-center  hover:underline"><Link href="/events/archive">Check out the Events Archive &gt;</Link></div>
+    
   </div>)
 }
