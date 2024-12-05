2a9e2bd443f533e3fbf0caf1d84788587a4a9c5d
rajsodhinyu <rs7152@nyu.edu>
2024-12-05 11:04:10 -0500

    second align for mobile

--- next-js/src/app/shop/product/tgs-hoodie/page.tsx
+++ next-js/src/app/shop/product/tgs-hoodie/page.tsx
@@ -150,12 +150,12 @@ export default function Post() {
               </div>
         </div>
         <br />
-        <div className="max-sm:w-screen">
+        <div className="max-sm:w-svh place-items-center">
           <div className="mt-5 text-center font-title font-bold
             text-4xl md:text-5xl text-tgs-purple">
                   Product Information
           </div>
-          <div className="text-3xl font-bit font-bold text-pretty w-5/6 place-self-center text-center py-12">
+          <div className="text-3xl font-bit font-bold text-pretty w-5/6 text-center py-12">
           The TGS Lounge Set is a two piece, mid-weight, fleece tracksuit featuring reverse appliqué artwork hand drawn by us.
             </div>
         </div>
