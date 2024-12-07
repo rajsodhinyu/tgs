b7431d5f26e8e4d00c87fcb400f18f72d62de7b4
rajsodhinyu <rs7152@nyu.edu>
2024-12-06 23:45:23 -0500

    scroll true

--- next-js/src/app/blog/shopCards.tsx
+++ next-js/src/app/blog/shopCards.tsx
@@ -5,8 +5,10 @@ import Link from "next/link";
 export default function ShopCard({ title, description, cover, url }: { title: string, description: string ,cover: string, url:string }) {
   return (
     <div className=" align-middle pb-3">
-     <Link className=""
-            href={url}> 
+     <Link className="" 
+            href={url}
+            scroll={true}
+            > 
         <img className='rounded-md place-self-center flex shrink -mt-2' src={cover} alt="" width={500} height={500}/>
       <div className="flex-col justify-between">
         <div className="pt-4 text-center text-pretty text-black font-bold font-bit text-3xl leading-none">{title}</div>
