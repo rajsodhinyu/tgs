40bb6c4f83aff9b9a12e861d6ca00ee29f4a7808
rajsodhinyu <rs7152@nyu.edu>
2024-12-09 12:03:03 -0500

    going live with new about

--- next-js/src/app/about/page.tsx
+++ next-js/src/app/about/page.tsx
@@ -3,7 +3,11 @@ import Image from "next/image"
 export default function Page() {
     return (<div className="mt-20 sm:px-40 mx-8">
         <div className="md:text-lg text-sm font-black font-bit text-pretty text-center sm:text-balance">
-            <p> That Good Sh*t! because good music transcends genre! Our mission is to build community around shared love of music. Through artist interviews, curated live events, playlists, and more, TGS is your go-to source for keeping up with the most important and innovative music of today!</p>
+            <p> That Good Sh*t! because good music transcends genre.
+
+Our mission is to build community around shared love of music. With authenticity at the core of everything we do, TGS is your go-to source for keeping up with the most impactful and innovative music of today!
+
+Join our growing community by keeping up with our artist interviews, curated live events, playlists, blog, and more.</p>
         </div>
         <div className="my-10 place-items-center">
             <Image width={1250} height={1000} className="w-full" src="https://cdn.sanity.io/images/fnvy29id/tgs/b5cee228b7299f1fd664e36a34f48678a30cb3d0-1250x1000.jpg" alt="" />
