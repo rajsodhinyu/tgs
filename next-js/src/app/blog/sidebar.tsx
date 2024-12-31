bff26fb623c69b655408ab16f538c17291c1fd46
rajsodhinyu <rs7152@nyu.edu>
2024-12-30 20:28:08 -1000

    added min width

--- next-js/src/app/blog/sidebar.tsx
+++ next-js/src/app/blog/sidebar.tsx
@@ -52,7 +52,7 @@ export default async function Sidebar({ items}: { items: number}) {
 
 
   return (
-    <div className="*:m-1 w-full">
+    <div className="*:m-1 w-full min-w-[300px]">
       <div className="font-bold hover:underline hover:text-tgs-purple decoration-tgs-purple text-black flex shrink text-4xl font-bit leading-10 text-nowrap justify-center">
         <Link href="/blog-archive">BLOG POSTS&gt;</Link>
         </div>
