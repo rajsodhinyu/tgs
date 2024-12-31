0e8d562bc3c0b91df1a590c1e7034a91f26e9347
rajsodhinyu <rs7152@nyu.edu>
2024-12-30 20:49:45 -1000

    restoring old

--- next-js/src/app/blog/sidebar.tsx
+++ next-js/src/app/blog/sidebar.tsx
@@ -52,18 +52,16 @@ export default async function Sidebar({ items}: { items: number}) {
 
 
   return (
-    <div className="*:m-1 max-[300px]:w-screen w-full">
-      <div className="font-bold hover:underline hover:text-tgs-purple decoration-tgs-purple text-black flex shrink text-4xl font-bit leading-10 text-nowrap justify-center">
+    <div className="*:m-1">
+      <div className="w-full font-bold hover:underline hover:text-tgs-purple decoration-tgs-purple text-black flex shrink text-4xl font-bit leading-10 text-nowrap justify-center">
         <Link href="/blog-archive">BLOG POSTS&gt;</Link>
         </div>
-      <div className="justify-items-center">
-        {truncatedBlogs.map((blog) => (
-          <div key={blog.name}>
-            <Link href={`${linkResolver(blog.youtube,blog.youtubeURL,blog.slug)}`} target={tabResolver(blog.youtube)}><Image className="lg:w-fit  border-opacity-0 hover:border-opacity-100 hover:scale-95 border-2 xl:border-4 border-tgs-purple rounded-md" src={`${eventImage(blog)}`} alt={`${blog.name} Cover`} width={400} height={400}></Image></Link>
-          </div>
-        ))}
-      </div>
-       <div className="font-bold hover:underline hover:text-tgs-purple decoration-tgs-purple text-black flex shrink text-4xl font-bit leading-10 text-nowrap justify-center">
+      {truncatedBlogs.map((blog) => (
+        <div key={blog.name}>
+          <Link href={`${linkResolver(blog.youtube,blog.youtubeURL,blog.slug)}`} target={tabResolver(blog.youtube)}><Image className="w-80 lg:w-fit  border-opacity-0 hover:border-opacity-100 hover:scale-95 border-2 xl:border-4 border-tgs-purple rounded-md" src={`${eventImage(blog)}`} alt={`${blog.name} Cover`} width={300} height={300}></Image></Link>
+        </div>
+      ))}
+       <div className="w-full font-bold hover:underline hover:text-tgs-purple decoration-tgs-purple text-black flex shrink text-4xl font-bit leading-10 text-nowrap justify-center">
         <Link href="/blog-archive">VIEW ALL&gt;</Link>
         </div>
     </div>
