c3f7d9e753cd3df087f67645d599d4b82a55a816
rajsodhinyu <rs7152@nyu.edu>
2025-03-12 13:07:50 -0400

    changed playlist text color to purple on hover!

--- next-js/src/app/blog/playlistCards.tsx
+++ next-js/src/app/blog/playlistCards.tsx
@@ -9,7 +9,7 @@ export default function PlaylistCard({ title, description, cover, url }: { title
         href={url}>
         <img className='rounded-lg place-self-center flex shrink border-tgs-purple border-0 group-hover:border-4 md:group-hover:border-6 group-hover:scale-[98%]' src={cover} alt="" />
         <div className="flex-col justify-between ">
-          <div className="pt-4 text-center text-pretty text-black text-xl font-bold font-bit leading-4 lg:text-3xl group-hover:underline decoration-tgs-purple">{title}</div>
+          <div className="pt-4 text-center text-pretty text-black group-hover:text-tgs-purple text-xl font-bold font-bit leading-4 lg:text-3xl group-hover:underline decoration-tgs-purple">{title}</div>
           <div className="w-11/12 place-self-center text-center align-center text-black group-hover:text-tgs-purple text-balance text-xs lg:text-lg font-semibold font-roc leading-none pt-2">{description}</div>
         </div>
       </Link>
