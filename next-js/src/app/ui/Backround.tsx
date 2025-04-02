5d78d0ddd2cb1e4ee05e1b95608543dac4132c5a
rajsodhinyu <rs7152@nyu.edu>
2025-04-01 21:54:42 -0400

    added new dial graphic for homepage

--- next-js/src/app/ui/Backround.tsx
+++ next-js/src/app/ui/Backround.tsx
@@ -189,7 +189,79 @@ const tight_coils = () => {
   myP5.remove;
   return <div id="test"></div>;
 };
+const dials = () => {
+  const s = (s: p5) => {
+    let width = s.windowWidth;
+    let height = s.windowHeight;
 
-const Backround = spiral_nomouse;
+    let gridSize = 40; // Size of each grid cell
+    let cols = width / gridSize;
+    let rows = height / gridSize;
+    let elasticity = 35; // Factor for mouse movement influence
+
+    s.preload = () => {};
+    s.windowResized = () => {
+      s.resizeCanvas(s.windowWidth, s.windowHeight);
+      width = s.windowWidth;
+      height = s.windowHeight;
+    };
+
+    s.setup = () => {
+      s.createCanvas(width, height);
+      s.background(0);
+      s.noStroke();
+    };
+
+    s.draw = () => {
+      s.background(26, 27, 35); // Black background
+      let path = s.getURLPath();
+      s.text(path.toString(), width / 2, height / 2);
+      {
+        // Loop through the grid
+        for (let i = 0; i < cols; i++) {
+          for (let j = 0; j < rows; j++) {
+            // Calculate x, y position
+            let x = i * gridSize;
+            let y = j * gridSize;
+
+            // Calculate offset based on mouse position
+            let dx = width / 1 - x;
+            let dy = height / 2 - y;
+            let dist = s.sqrt(dx * dx + dy * dy);
+
+            // Offset based on elasticity
+            let offsetX = s.atan(s.frameCount * 0.01 + dist);
+            let offsetY = s.tan(s.frameCount * 0.01 + dist);
+            //let offsetX = s.atan(s.frameCount * 0.01 + dist * 0.1);
+            // let offsetY = s.tan(s.frameCount * 0.001 + dist * 0.03);
+
+            // Alternate colors
+            if ((i + j) % 5 === 0) {
+              s.fill(237, 157, 249); // Pink
+            } else {
+              s.fill(108, 92, 190); // Purple
+            }
+
+            // Draw the rectangle with offset
+            let dial = s.map(
+              s.sin(s.frameCount * 0.01),
+              -1,
+              1,
+              0.1,
+              0.02,
+              true,
+            );
+            // s.frameCount * 0.01 * 0.05
+            s.rect(x + offsetX, y + offsetY, gridSize * dial, gridSize);
+          }
+        }
+      }
+    };
+  };
+  const myP5 = new p5(s);
+  myP5.remove;
+  return <div id="test"></div>;
+};
+const Backround = dials;
 
 export default Backround;
