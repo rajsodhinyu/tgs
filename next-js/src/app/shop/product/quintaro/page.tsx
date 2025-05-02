2c114afc95f5632d0abdd61ade327810e2a0059a
rajsodhinyu <rs7152@nyu.edu>
2025-05-02 18:33:35 -0400

    removed print pages and finished Quintaro

--- next-js/src/app/shop/product/quintaro/page.tsx
+++ next-js/src/app/shop/product/quintaro/page.tsx
@@ -4,10 +4,18 @@ import { useState } from "react";
 import Form from "next/form";
 
 const cards = [
-  "https://place-hold.it/800x1000.jpeg",
-  "https://place-hold.it/800x1000.jpeg",
-  "https://place-hold.it/800x1000.jpeg",
-  "https://place-hold.it/800x1000.jpeg",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/c701b2099e1b43b7f672e85e6a425692ec0ac1d1-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/fd3bcb361dee60f3210141579ceaccf4a59ae830-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/06a07e85a9cf836247167f94ef19be4616c70e1e-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/c925c377687bf7dfbce08c9e9a733b3033e88f7d-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/c701b2099e1b43b7f672e85e6a425692ec0ac1d1-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/fd3bcb361dee60f3210141579ceaccf4a59ae830-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/06a07e85a9cf836247167f94ef19be4616c70e1e-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/c925c377687bf7dfbce08c9e9a733b3033e88f7d-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/c701b2099e1b43b7f672e85e6a425692ec0ac1d1-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/fd3bcb361dee60f3210141579ceaccf4a59ae830-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/06a07e85a9cf836247167f94ef19be4616c70e1e-2726x3408.png?h=1000",
+  "https://cdn.sanity.io/images/fnvy29id/tgs/c925c377687bf7dfbce08c9e9a733b3033e88f7d-2726x3408.png?h=1000",
 ];
 
 interface HorizontalCarouselProps {
@@ -153,8 +161,28 @@ export default function Post() {
         >
           Product Information
         </div>
-        <div className="text-3xl font-bit font-bold text-pretty sm:w-5/6 text-center py-12">
-          yeah bruh idk what to put here
+        <div className="text-2xl font-bit font-bold text-pretty sm:w-5/6 text-left py-12">
+          <p>Quincy Davis - Quintaro</p>
+          <p>Format: DVD-R</p>
+          <p>Runtime: 28m</p>
+          <p>
+            <br></br>
+          </p>
+          <p>
+            Quintaro is about moving forward, climbing impossible heights, and
+            perseverance through daydreams. The visualizer for this album is a
+            biking edit, a surrealistic glitch collage, a multi-multi-media
+            piece created by Quincy Davis and Raj Sodhi. The DVD was made for
+            you to put into your Playstation when you’re on your couch or with
+            the homies while you work or relax.
+          </p>
+          <p>
+            <br></br>
+          </p>
+          <p>Album: Quincy Davis</p>
+          <p>Edit/VFX: Raj Sodhi (@juulwry) + Quincy Davis (@quincydaviss)</p>
+          <p>Footage: Raj Sodhi + Ria Mehrotra (@ria.mehrotra)</p>
+          <p>Album and DVD Covers: Virgil Warren (@vw_studio5)</p>
         </div>
       </div>
     </div>
