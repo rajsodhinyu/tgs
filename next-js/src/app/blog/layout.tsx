44a8fbaf673a73a58e2611fe098d907566b0774d
rajsodhinyu <rs7152@nyu.edu>
2025-05-03 20:33:54 -0400

    restructedd bottom margin

--- next-js/src/app/blog/layout.tsx
+++ next-js/src/app/blog/layout.tsx
@@ -2,7 +2,7 @@ import Nav from "../nav";
 import Sidebar from "./sidebar";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (<div className="min-w-80">
+    return (<div className="min-w-80 mb-10">
         <Nav />
         <div className="w-full px-3 sm:flex-wrap gap-2.5 md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center "> 
   {/* Whole Site */}
--- next-js/src/app/events/layout.tsx
+++ next-js/src/app/events/layout.tsx
@@ -2,7 +2,7 @@ import Nav from "../nav";
 
 
 export default function Layout({ children }: { children: React.ReactNode }) {
-    return (<div>
+    return (<div className="mb-10">
         <Nav />
         {children}
     </div>
