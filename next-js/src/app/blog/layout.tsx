0b9471a0a5858a2fb3c8198a9cb3c9cb07672e1b
rajsodhinyu <rs7152@nyu.edu>
2025-05-12 16:12:20 -0400

    added w-full back to children

--- next-js/src/app/blog/layout.tsx
+++ next-js/src/app/blog/layout.tsx
@@ -6,7 +6,7 @@ export default function Layout({ children }: { children: React.ReactNode }) {
     <div className="min-w-80 mb-10">
       <Nav />
       <div className="w-full px-3 sm:flex-wrap md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center">
-        <div>{children}</div>
+        <div className="w-full">{children}</div>
         <div>
           <Sidebar />
         </div>
