eee73f6af9d1e7cc6051c90de6f9fad50fd124ad
rajsodhinyu <rs7152@nyu.edu>
2025-05-12 15:47:40 -0400

    changed width bug

--- next-js/src/app/blog/layout.tsx
+++ next-js/src/app/blog/layout.tsx
@@ -5,9 +5,9 @@ export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div className="min-w-80 mb-10">
       <Nav />
-      <div className="w-full pl-1 sm:flex-wrap md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center ">
-        <div className="w-full">{children}</div>
-        <div className="">
+      <div className="w-full px-3 sm:flex-wrap md:flex-nowrap md:inline-flex md:flex-row md:justify-between xl:w-screen max-md:place-items-center">
+        <div>{children}</div>
+        <div>
           <Sidebar />
         </div>
       </div>
