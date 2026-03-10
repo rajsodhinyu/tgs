afc98799b01ee88aef92ea760b194792bcd93b16
Raj Sodhi <rs7152@nyu.edu>
2026-03-10 11:18:53 -0400

    scaling sotd

--- next-js/src/app/ui/Sotd.tsx
+++ next-js/src/app/ui/Sotd.tsx
@@ -13,10 +13,11 @@ export default function Sotd() {
   const sotdata = useSotData();
   return (
     <div className="group" onClick={catchClick}>
-      <div className="w-full md:w-72 md:ml-4 xl:flex-none rounded-xl inline-flex place-items-center align-center justify-around">
+      <div className="w-full md:w-72 md:ml-4 xl:flex-none rounded-xl inline-flex place-items-center align-center justify-end">
         <img
           src="https://cdn.sanity.io/images/fnvy29id/tgs/d57f341e73494edac6d2ef1a03574243adb21afb-2010x516.png"
           alt=""
+          className="max-w-56"
         />
       </div>
       <div>
