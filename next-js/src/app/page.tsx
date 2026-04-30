65d19373b0b4e56bfb20a524f6e3faf2b558af0a
Raj Sodhi <rs7152@nyu.edu>
2026-04-30 01:34:06 -0400

    switch to checkerboard

--- next-js/src/app/page.tsx
+++ next-js/src/app/page.tsx
@@ -7,7 +7,7 @@ import { useSotData } from "./context/SotDataContext";
 import Link from "next/link";
 
 const DynamicComponentWithNoSSR = dynamic(
-  () => import("./ui/WaveformBackground"),
+  () => import("./ui/CheckerboardBackground"),
   {
     ssr: false,
     loading: () => (
